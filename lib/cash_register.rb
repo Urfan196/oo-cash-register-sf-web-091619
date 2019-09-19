7d539f2e04ea04173fe38a860328e08433d366eb
Urfan Guliyev <urfan196@yahoo.com>
2019-09-19 19:52:24 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,29 @@
+class CashRegister
 
+attr_accessor :total, :discount
+
+def initialize (discount)
+  @total = 0
+  @discount = discount
+  @items= []
+end
+
+def add_item (title, price, quantity)
+  @total += price * quantity
+  @items << title
+end
+
+def apply_discount
+  if @discount != 0 do
+    @total -= @total * @discount / 100.0
+    puts "After the discount, the total comes to #{@total}."
+  else
+    puts "There is no discount to apply."
+  end
+end
+
+def items
+  @items
+end
+
+end
