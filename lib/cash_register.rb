ba236f0ea15c0a027a0c8666fb136733bd12fb15
Urfan Guliyev <urfan196@yahoo.com>
2019-09-19 19:59:52 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,41 +1,40 @@
 class CashRegister
 
-attr_accessor :total, :discount, :items, :price, :quantity
+  attr_accessor :total, :discount, :items
 
-def initialize (discount)
-  @total = 0
-  @discount = discount
-  @items= []
-end
-
-def add_item (title, price, quantity)
-    @price = price
-    @quantity = quantity
-  @total += price * quantity
-  @items << title
-end
-
-def apply_discount
-  if @discount != 0 do
-    @total -= @total * @discount / 100.0
-    puts "After the discount, the total comes to #{@total}."
-  else
-    puts "There is no discount to apply."
+  def initialize (discount)
+    @total = 0
+    @discount = discount
+    @items= []
   end
-end
 
-def items
-  @items
-end
+  def add_item (title, price, quantity)
+      @price = price
+      @quantity = quantity
+    @total += price * quantity
+    @items << title
+  end
 
-def void_last_transaction
-  @items.delete_at(-1)
-if @items.size == 0
-  @total == 0.0
-else
-  @total -= @price * @quantity
-end
+  def apply_discount
+    if @discount != 0 do
+      @total -= @total * @discount / 100.0
+      puts "After the discount, the total comes to #{@total}."
+    else
+      puts "There is no discount to apply."
+    end
+  end
 
-end
+  def items
+    @items
+  end
+
+  def void_last_transaction
+    @items.delete_at(-1)
+    if @items.size == 0
+      @total == 0.0
+    else
+      @total -= @price * @quantity
+    end
+  end
 
 end
